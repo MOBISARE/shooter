f37dccdeff24c585f5e9900f9712f9941e201829
Maxime MOSELLE <moselle@gigabyte.localdomain>
2021-11-13 20:06:34 +0100

    Ajour PyDoc sur main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from game import Game
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    """
+    Point d'entré du programme.
+    """
     print("Launching...")
     pygame.init()
     game = Game()
